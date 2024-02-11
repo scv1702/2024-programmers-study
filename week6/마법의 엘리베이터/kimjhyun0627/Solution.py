b615193b03bb3dbf2701cc769eba2b0da17a6264
김진현 <79571893+kimjhyun0627@users.noreply.github.com>
2024-02-11 06:57:46 +0000

    solve: week6 - 마법의이엘리베이터

---
+++ week6/마법의 엘리베이터/kimjhyun0627/Solution.py
@@ -0,0 +1,25 @@
+def solution(storey):
+
+    answer = 0
+    while storey:
+        move = storey % 10
+        if move > 5:
+            answer += (10 - move)
+            storey += (10 - move)
+        elif move < 5:
+            answer += move
+            storey -= move
+        else:
+            tmp = storey // 10
+            if tmp % 10 >= 5:
+                answer += (10 - move)
+                storey += (10 - move)
+            else:
+                answer += move
+                storey -= move
+        storey //= 10
+
+    return answer
+
+print(solution(16))
+print(solution(2554))
